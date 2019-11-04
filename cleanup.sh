5941ff053bb592ecd1718dc1809fe153ce5ab23e
Sam Thursfield <sam@afuera.me.uk>
2019-11-05 00:57:05 +0100

    Use system provided Tracker
    Since Tracker 2.2, the API needed to run it on a private DBus connection
    is available so we don't need to build from source.

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-dnf remove -y autoconf automake cracklib-dicts dbus-devel gcc gtk2 gtk-doc libseccomp-devel libvorbis-devel libxml2-devel NetworkManager-libnm-devel python3-devel redhat-rpm-config
+dnf remove -y gcc gtk2 python3-devel redhat-rpm-config
 dnf clean all
 
 rm -R /root/*
